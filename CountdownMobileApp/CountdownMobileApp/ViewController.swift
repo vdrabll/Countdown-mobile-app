cd50eca09463d07807101e75c3916ce96796e9d5
FoxyBooks <viktorya.fedosova2016@yandex.ru>
2022-04-29 18:52:51 +0300

    create skeleton project

---
+++ CountdownMobileApp/CountdownMobileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountdownMobileApp
+//
+//  Created by Виктория Федосова on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
